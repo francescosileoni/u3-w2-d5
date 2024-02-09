0758da6f826df1cf65691fd48c9787b491691e1a
Francesco Sileoni <francesco.sile@gmail.com>
2024-02-09 17:09:47 +0100

    nuovo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import DetailsPage from './components/DetailsPage';
-import CustomNavbar from './components/CustomNavbar';
+// import CustomNavbar from './components/CustomNavbar';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <CustomNavbar />
+        {/* <CustomNavbar />  non mi funziona me ne importa 2 */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/details/:city" element={<DetailsPage />} />
